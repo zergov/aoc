f1603b286ec5b3341107f3a62b7677e0ff1df92f
Jonathan Lalande <jonathan.lalande@shopify.com>
2018-12-08 20:26:58 -0500

    submit

--- lib/aoc/challenge.rb
+++ lib/aoc/challenge.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'net/http'
 require 'open-uri'
 
 module Aoc
@@ -28,9 +29,47 @@ module Aoc
       content.css('article.day-desc').count > 1
     end
 
+    def submit(answer)
+      unless completed?
+        current_level = content.at('input[@name="level"]')["value"]
+
+        uri = URI.parse("#{url}/answer")
+        http = Net::HTTP.new(uri.host, 80)
+
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request["Cookie"] = "session=#{Aoc::Session.get_session}"
+        request.set_form_data({level: current_level, answer: answer})
+
+        response = http.request(request)
+
+        case response
+        when Net::HTTPSuccess then
+          response.body
+        when Net::HTTPMovedPermanently then
+          location = response['location']
+          warn "redirected to #{location}"
+
+          uri = URI.parse(location)
+          http = Net::HTTP.new(uri.host, 80)
+
+          request = Net::HTTP::Post.new(uri.request_uri)
+          request["Cookie"] = "session=#{Aoc::Session.get_session}"
+          http.request(request).body
+        else
+          response
+        end
+      else
+        "This challenge is already completed!"
+      end
+    end
+
     def input
       session = Aoc::Session.get_session
       @input ||= Nokogiri::HTML(open("#{url}/input", "Cookie" => "session=#{session}")).text
+    end
+
+    def completed?
+      content.text.include?("Both parts of this puzzle are complete!")
     end
 
     private
